9b1f36ddc62b2bbbe611b6aa29c58493fbda568d
Takumi Ikegawa <90892606+trailstem@users.noreply.github.com>
2024-11-19 18:53:30 +0900

    production deploy (#4)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>helloworld</h1>
+      <h1>helloworld hoge fuga piyo pupupu</h1>
     </div>
   );
 }
